d14ceab83ed877c36269e94f8b120b427b69a85f
Bruno <bruno.sant.a@gmail.com>
2017-09-14 14:22:34 -0300

    Inicio do codigo

--- PHP/requisicao.php
+++ PHP/requisicao.php
@@ -1,2 +1,16 @@
 <?
+$resposta = 'False';
+if (isset($_GET['UID'])){
+	$_uid = $_GET['UID'];
+
+	$json_file = fopen('cadastro.json', 'r');
+	$json_file = fread($json_file,filesize('cadastro.json'));
+	$json = json_decode($json_file, true);
+
+
+	#TODO
+
+}
+
+echo $resposta;
 ?>
